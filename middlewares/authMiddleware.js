39abd5cb3c064964ca08575bba35c010c470ff95
Santanu Garnaik <96882645+SantanuGarnaik@users.noreply.github.com>
2024-07-02 06:06:00 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,17 +1,32 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const authMiddleware = (req, res, next) => {
-  // Get the token from the request cookies
-  const token = req.cookies.access_token;
+  // Check for token in cookies
+  const cookieToken = req.cookies.access_token;
 
+  // Check for token in Authorization header
+  const authHeader = req.headers.authorization;
+  let headerToken;
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    headerToken = authHeader.split(" ")[1];
+  }
+
+  // Use token from cookies if present, otherwise use token from header
+  const token = cookieToken || headerToken;
+  console.log(token);
   if (!token) {
-    return res.status(401).json({ message: 'Authentication token is missing' });
+    // If no token is found
+    return res.status(401).json({ message: "Authentication token is missing" });
   }
 
   // Verify the token
-  jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decodedToken) => {
+    console.log("secret", process.env.JWT_SECRET_KEY);
+    console.error(err);
     if (err) {
-      return res.status(401).json({ message: 'Invalid token' });
+      return res.status(401).json({ message: "Invalid token" });
     }
     // Attach user ID to request object for further use
     req.userId = decodedToken.id;
